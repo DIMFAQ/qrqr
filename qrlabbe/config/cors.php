5db0dc416e644a1f1b684b362d4dbfc1394d0505
Dimas Faqih <dimasfaqih005@gmail.com>
2025-10-31 23:24:50 +0700

    first commit

---
+++ qrlabbe/config/cors.php
@@ -0,0 +1,3 @@
+'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'], // Tambahkan 'login' dan 'logout'
+'allowed_origins' => ['http://localhost:3000'], // Alamat React kamu
+'supports_credentials' => true, // WAJIB!
